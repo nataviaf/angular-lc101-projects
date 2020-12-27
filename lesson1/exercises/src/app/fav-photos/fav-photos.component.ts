9c5a072ce049ed2212fbd344dc0f4352ba2b0cda
Natavia <natavia.finnie@gmail.com>
2020-12-27 14:01:40 -0600

    init commit

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Favorite Photos';
   image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://static.independent.co.uk/s3fs-public/thumbnails/image/2018/06/19/16/garfield-mondays.jpg?width=990';
+  image3 = 'https://www.gannett-cdn.com/presto/2019/12/19/PBIN/c15dd0b9-ee21-4a95-903a-07650cc37d9c-Lucy.jpg?width=660&height=495&fit=crop&format=pjpg&auto=webp';
 
   constructor() { }
 
